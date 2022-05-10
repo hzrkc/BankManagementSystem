312323fd38ecccf0bd6f2eec57199246da82f049
Hazar Koc <hazark41@gmail.com>
2022-05-10 17:42:44 +0300

    added images

--- BankaManagementSystem/BankaManagementSystem/BankaMüdürü.Designer.cs
+++ BankaManagementSystem/BankaManagementSystem/BankaMüdürü.Designer.cs
@@ -59,9 +59,8 @@
             this.panel1.Controls.Add(this.panel2);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1067, 617);
+            this.panel1.Size = new System.Drawing.Size(800, 501);
             this.panel1.TabIndex = 1;
             // 
             // panel6
@@ -70,21 +69,19 @@
             this.panel6.Controls.Add(this.panel7);
             this.panel6.Controls.Add(this.dataGridView4);
             this.panel6.Controls.Add(this.panel8);
-            this.panel6.Location = new System.Drawing.Point(185, 68);
-            this.panel6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel6.Location = new System.Drawing.Point(139, 55);
             this.panel6.Name = "panel6";
-            this.panel6.Size = new System.Drawing.Size(859, 519);
+            this.panel6.Size = new System.Drawing.Size(644, 422);
             this.panel6.TabIndex = 9;
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 372);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Location = new System.Drawing.Point(0, 302);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.Size = new System.Drawing.Size(859, 128);
+            this.dataGridView1.Size = new System.Drawing.Size(644, 104);
             this.dataGridView1.TabIndex = 12;
             // 
             // panel7
@@ -92,19 +89,17 @@
             this.panel7.Controls.Add(this.label4);
             this.panel7.Controls.Add(this.label5);
             this.panel7.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel7.Location = new System.Drawing.Point(0, 219);
-            this.panel7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel7.Location = new System.Drawing.Point(0, 178);
             this.panel7.Name = "panel7";
-            this.panel7.Size = new System.Drawing.Size(859, 153);
+            this.panel7.Size = new System.Drawing.Size(644, 124);
             this.panel7.TabIndex = 11;
             // 
             // label4
             // 
             this.label4.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label4.Location = new System.Drawing.Point(207, 80);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(155, 65);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(473, 44);
+            this.label4.Size = new System.Drawing.Size(355, 36);
             this.label4.TabIndex = 9;
             this.label4.Text = "\" X ADET İŞLEM \"";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -112,10 +107,9 @@
             // label5
             // 
             this.label5.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label5.Location = new System.Drawing.Point(207, 27);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(155, 22);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(473, 44);
+            this.label5.Size = new System.Drawing.Size(355, 36);
             this.label5.TabIndex = 8;
             this.label5.Text = "Banka İşlemleri";
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -124,11 +118,10 @@
             // 
             this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView4.Dock = System.Windows.Forms.DockStyle.Top;
-            this.dataGridView4.Location = new System.Drawing.Point(0, 91);
-            this.dataGridView4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView4.Location = new System.Drawing.Point(0, 74);
             this.dataGridView4.Name = "dataGridView4";
             this.dataGridView4.RowHeadersWidth = 51;
-            this.dataGridView4.Size = new System.Drawing.Size(859, 128);
+            this.dataGridView4.Size = new System.Drawing.Size(644, 104);
             this.dataGridView4.TabIndex = 10;
             // 
             // panel8
@@ -136,24 +129,23 @@
             this.panel8.Controls.Add(this.label6);
             this.panel8.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel8.Location = new System.Drawing.Point(0, 0);
-            this.panel8.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel8.Name = "panel8";
-            this.panel8.Size = new System.Drawing.Size(859, 91);
+            this.panel8.Size = new System.Drawing.Size(644, 74);
             this.panel8.TabIndex = 9;
             // 
             // label6
             // 
             this.label6.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label6.Location = new System.Drawing.Point(207, 20);
-            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Location = new System.Drawing.Point(155, 16);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(473, 44);
+            this.label6.Size = new System.Drawing.Size(355, 36);
             this.label6.TabIndex = 7;
             this.label6.Text = "Banka Genel Durum";
             this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // panel2
             // 
+            this.panel2.BackColor = System.Drawing.Color.Fuchsia;
             this.panel2.Controls.Add(this.button2);
             this.panel2.Controls.Add(this.button4);
             this.panel2.Controls.Add(this.button5);
@@ -161,9 +153,8 @@
             this.panel2.Controls.Add(this.panel5);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel2.Location = new System.Drawing.Point(0, 0);
-            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(167, 617);
+            this.panel2.Size = new System.Drawing.Size(125, 501);
             this.panel2.TabIndex = 0;
             // 
             // button2
@@ -172,10 +163,9 @@
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(25)))), ((int)(((byte)(76)))));
-            this.button2.Location = new System.Drawing.Point(0, 413);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Location = new System.Drawing.Point(0, 335);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(167, 105);
+            this.button2.Size = new System.Drawing.Size(125, 85);
             this.button2.TabIndex = 11;
             this.button2.Text = "Sistem Güncelleme";
             this.button2.UseVisualStyleBackColor = true;
@@ -186,10 +176,9 @@
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(25)))), ((int)(((byte)(76)))));
-            this.button4.Location = new System.Drawing.Point(0, 308);
-            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button4.Location = new System.Drawing.Point(0, 250);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(167, 105);
+            this.button4.Size = new System.Drawing.Size(125, 85);
             this.button4.TabIndex = 10;
             this.button4.Text = "Müşteri Kayıt";
             this.button4.UseVisualStyleBackColor = true;
@@ -200,10 +189,9 @@
             this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.button5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(25)))), ((int)(((byte)(76)))));
-            this.button5.Location = new System.Drawing.Point(0, 203);
-            this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button5.Location = new System.Drawing.Point(0, 165);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(167, 105);
+            this.button5.Size = new System.Drawing.Size(125, 85);
             this.button5.TabIndex = 9;
             this.button5.Text = "Yatırım Fonu";
             this.button5.UseVisualStyleBackColor = true;
@@ -214,10 +202,9 @@
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(25)))), ((int)(((byte)(76)))));
-            this.button3.Location = new System.Drawing.Point(0, 98);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Location = new System.Drawing.Point(0, 80);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(167, 105);
+            this.button3.Size = new System.Drawing.Size(125, 85);
             this.button3.TabIndex = 8;
             this.button3.Text = "Kredi İşlemleri";
             this.button3.UseVisualStyleBackColor = true;
@@ -226,19 +213,17 @@
             // 
             this.panel5.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel5.Location = new System.Drawing.Point(0, 0);
-            this.panel5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(167, 98);
+            this.panel5.Size = new System.Drawing.Size(125, 80);
             this.panel5.TabIndex = 7;
             // 
             // BankaMüdürü
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 615);
+            this.ClientSize = new System.Drawing.Size(800, 500);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "BankaMüdürü";
             this.Text = "BankaMüdürü";
             this.panel1.ResumeLayout(false);
