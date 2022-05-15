71fdf00df17b7828f60b142131d94b3f0c3dcd4c
Asli <200202047@kocaeli.edu.tr>
2022-05-15 22:00:31 +0300

    back up ve form detay

--- BankaManagementSystem/BankaManagementSystem/Menu_BankaMüdürü/SistemGüncelleme.Designer.cs
+++ BankaManagementSystem/BankaManagementSystem/Menu_BankaMüdürü/SistemGüncelleme.Designer.cs
@@ -29,124 +29,190 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.button2 = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.btn_Guncelle = new System.Windows.Forms.Button();
+            this.lbl_maas = new System.Windows.Forms.Label();
+            this.TxtBox_Maas = new System.Windows.Forms.TextBox();
+            this.txtBox_ID = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.dgv_bankaUyeleri = new System.Windows.Forms.DataGridView();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Dgv_Sistem = new System.Windows.Forms.DataGridView();
+            this.pnl_label = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
-            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_bankaUyeleri)).BeginInit();
             this.panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv_Sistem)).BeginInit();
+            this.pnl_label.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(239)))), ((int)(((byte)(255)))));
-            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.btn_Guncelle);
+            this.panel1.Controls.Add(this.lbl_maas);
+            this.panel1.Controls.Add(this.TxtBox_Maas);
+            this.panel1.Controls.Add(this.txtBox_ID);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.dgv_bankaUyeleri);
             this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.Dgv_Sistem);
+            this.panel1.Controls.Add(this.pnl_label);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(876, 556);
             this.panel1.TabIndex = 1;
             // 
-            // panel3
+            // btn_Guncelle
             // 
-            this.panel3.Controls.Add(this.button2);
-            this.panel3.Controls.Add(this.label2);
-            this.panel3.Controls.Add(this.maskedTextBox1);
-            this.panel3.Controls.Add(this.label3);
-            this.panel3.Location = new System.Drawing.Point(16, 327);
-            this.panel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(703, 150);
-            this.panel3.TabIndex = 3;
+            this.btn_Guncelle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(223)))), ((int)(((byte)(255)))));
+            this.btn_Guncelle.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Guncelle.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(175)))), ((int)(((byte)(255)))));
+            this.btn_Guncelle.FlatAppearance.BorderSize = 2;
+            this.btn_Guncelle.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(215)))), ((int)(((byte)(255)))));
+            this.btn_Guncelle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Guncelle.Font = new System.Drawing.Font("Microsoft JhengHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Guncelle.Location = new System.Drawing.Point(231, 518);
+            this.btn_Guncelle.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_Guncelle.Name = "btn_Guncelle";
+            this.btn_Guncelle.Size = new System.Drawing.Size(370, 34);
+            this.btn_Guncelle.TabIndex = 37;
+            this.btn_Guncelle.Text = "Güncelle";
+            this.btn_Guncelle.UseVisualStyleBackColor = false;
+            this.btn_Guncelle.Click += new System.EventHandler(this.btn_Guncelle_Click);
             // 
-            // button2
+            // lbl_maas
             // 
-            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(140)))), ((int)(((byte)(204)))));
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Bold);
-            this.button2.Location = new System.Drawing.Point(245, 90);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(133, 47);
-            this.button2.TabIndex = 11;
-            this.button2.Text = ">>>";
-            this.button2.UseVisualStyleBackColor = true;
+            this.lbl_maas.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_maas.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(52)))), ((int)(((byte)(76)))));
+            this.lbl_maas.Location = new System.Drawing.Point(483, 479);
+            this.lbl_maas.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_maas.Name = "lbl_maas";
+            this.lbl_maas.Size = new System.Drawing.Size(82, 31);
+            this.lbl_maas.TabIndex = 22;
+            this.lbl_maas.Text = "Maas :";
             // 
-            // label2
+            // TxtBox_Maas
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Arial Narrow", 15F, System.Drawing.FontStyle.Bold);
-            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(52)))), ((int)(((byte)(76)))));
-            this.label2.Location = new System.Drawing.Point(12, 96);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label2.Name = "label2";
-            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.label2.Size = new System.Drawing.Size(208, 30);
-            this.label2.TabIndex = 9;
-            this.label2.Text = "Sistemi 1 Ay İlerlet:";
+            this.TxtBox_Maas.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(223)))), ((int)(((byte)(255)))));
+            this.TxtBox_Maas.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TxtBox_Maas.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtBox_Maas.Location = new System.Drawing.Point(562, 478);
+            this.TxtBox_Maas.Name = "TxtBox_Maas";
+            this.TxtBox_Maas.Size = new System.Drawing.Size(221, 27);
+            this.TxtBox_Maas.TabIndex = 23;
             // 
-            // maskedTextBox1
+            // txtBox_ID
             // 
-            this.maskedTextBox1.Location = new System.Drawing.Point(245, 47);
-            this.maskedTextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.maskedTextBox1.Mask = "00/00/0000 90:00";
-            this.maskedTextBox1.Name = "maskedTextBox1";
-            this.maskedTextBox1.Size = new System.Drawing.Size(132, 22);
-            this.maskedTextBox1.TabIndex = 8;
-            this.maskedTextBox1.ValidatingType = typeof(System.DateTime);
+            this.txtBox_ID.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(223)))), ((int)(((byte)(255)))));
+            this.txtBox_ID.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtBox_ID.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBox_ID.Location = new System.Drawing.Point(101, 479);
+            this.txtBox_ID.Name = "txtBox_ID";
+            this.txtBox_ID.Size = new System.Drawing.Size(221, 27);
+            this.txtBox_ID.TabIndex = 26;
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Arial Narrow", 15F, System.Drawing.FontStyle.Bold);
+            this.label3.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(52)))), ((int)(((byte)(76)))));
-            this.label3.Location = new System.Drawing.Point(67, 42);
+            this.label3.Location = new System.Drawing.Point(51, 479);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.label3.Size = new System.Drawing.Size(153, 30);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "Sistem Tarihi:";
+            this.label3.Size = new System.Drawing.Size(43, 31);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "ID :";
+            // 
+            // dgv_bankaUyeleri
+            // 
+            this.dgv_bankaUyeleri.AllowUserToAddRows = false;
+            this.dgv_bankaUyeleri.AllowUserToDeleteRows = false;
+            this.dgv_bankaUyeleri.AllowUserToOrderColumns = true;
+            this.dgv_bankaUyeleri.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv_bankaUyeleri.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(223)))), ((int)(((byte)(255)))));
+            this.dgv_bankaUyeleri.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgv_bankaUyeleri.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.RaisedHorizontal;
+            this.dgv_bankaUyeleri.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_bankaUyeleri.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dgv_bankaUyeleri.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(239)))), ((int)(((byte)(255)))));
+            this.dgv_bankaUyeleri.Location = new System.Drawing.Point(0, 279);
+            this.dgv_bankaUyeleri.Margin = new System.Windows.Forms.Padding(4);
+            this.dgv_bankaUyeleri.MultiSelect = false;
+            this.dgv_bankaUyeleri.Name = "dgv_bankaUyeleri";
+            this.dgv_bankaUyeleri.ReadOnly = true;
+            this.dgv_bankaUyeleri.RowHeadersWidth = 51;
+            this.dgv_bankaUyeleri.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgv_bankaUyeleri.Size = new System.Drawing.Size(876, 175);
+            this.dgv_bankaUyeleri.TabIndex = 35;
             // 
             // panel2
             // 
-            this.panel2.Controls.Add(this.dataGridView1);
-            this.panel2.Controls.Add(this.label1);
-            this.panel2.Location = new System.Drawing.Point(16, 27);
-            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel2.BackColor = System.Drawing.Color.Transparent;
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 227);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(703, 268);
-            this.panel2.TabIndex = 2;
+            this.panel2.Size = new System.Drawing.Size(876, 52);
+            this.panel2.TabIndex = 34;
             // 
-            // dataGridView1
+            // label2
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(17, 63);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.Size = new System.Drawing.Size(667, 185);
-            this.dataGridView1.TabIndex = 1;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label2.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(52)))), ((int)(((byte)(76)))));
+            this.label2.Location = new System.Drawing.Point(0, 21);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(876, 31);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Banka çalışanları ve maaşları ";
+            // 
+            // Dgv_Sistem
+            // 
+            this.Dgv_Sistem.AllowUserToAddRows = false;
+            this.Dgv_Sistem.AllowUserToDeleteRows = false;
+            this.Dgv_Sistem.AllowUserToOrderColumns = true;
+            this.Dgv_Sistem.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.Dgv_Sistem.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(223)))), ((int)(((byte)(255)))));
+            this.Dgv_Sistem.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Dgv_Sistem.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.RaisedHorizontal;
+            this.Dgv_Sistem.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Dgv_Sistem.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Dgv_Sistem.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(239)))), ((int)(((byte)(255)))));
+            this.Dgv_Sistem.Location = new System.Drawing.Point(0, 52);
+            this.Dgv_Sistem.Margin = new System.Windows.Forms.Padding(4);
+            this.Dgv_Sistem.MultiSelect = false;
+            this.Dgv_Sistem.Name = "Dgv_Sistem";
+            this.Dgv_Sistem.ReadOnly = true;
+            this.Dgv_Sistem.RowHeadersWidth = 51;
+            this.Dgv_Sistem.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.Dgv_Sistem.Size = new System.Drawing.Size(876, 175);
+            this.Dgv_Sistem.TabIndex = 33;
+            // 
+            // pnl_label
+            // 
+            this.pnl_label.BackColor = System.Drawing.Color.Transparent;
+            this.pnl_label.Controls.Add(this.label1);
+            this.pnl_label.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnl_label.Location = new System.Drawing.Point(0, 0);
+            this.pnl_label.Name = "pnl_label";
+            this.pnl_label.Size = new System.Drawing.Size(876, 52);
+            this.pnl_label.TabIndex = 32;
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold);
+            this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label1.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(52)))), ((int)(((byte)(76)))));
-            this.label1.Location = new System.Drawing.Point(11, 11);
+            this.label1.Location = new System.Drawing.Point(0, 21);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(338, 35);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Güncel Sistem Bilgileri";
+            this.label1.Size = new System.Drawing.Size(876, 31);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Sistem Güncelleme ";
             // 
             // SistemGüncelleme
             // 
@@ -160,11 +226,11 @@
             this.Text = "SistemGüncellemecs";
             this.Load += new System.EventHandler(this.SistemGüncelleme_Load);
             this.panel1.ResumeLayout(false);
-            this.panel3.ResumeLayout(false);
-            this.panel3.PerformLayout();
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_bankaUyeleri)).EndInit();
             this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv_Sistem)).EndInit();
+            this.pnl_label.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -172,13 +238,16 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel pnl_label;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView Dgv_Sistem;
+        private System.Windows.Forms.DataGridView dgv_bankaUyeleri;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBox_ID;
+        private System.Windows.Forms.TextBox TxtBox_Maas;
+        private System.Windows.Forms.Label lbl_maas;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_Guncelle;
     }
 }
--- BankaManagementSystem/BankaManagementSystem/Menu_BankaMüdürü/SistemGüncelleme.cs
+++ BankaManagementSystem/BankaManagementSystem/Menu_BankaMüdürü/SistemGüncelleme.cs
@@ -36,6 +36,84 @@ namespace BankaManagementSystem.Menu_BankaMüdürü
         private void SistemGüncelleme_Load(object sender, EventArgs e)
         {
             conn = new NpgsqlConnection(connstring);
+            Sistem_Select();
+            Banka_Select();
+        }
+
+        private void Sistem_Select()
+        {
+            try
+            {
+                conn.Open();
+                sql = @"SELECT * FROM sistem ";
+
+                cmd = new NpgsqlCommand(sql, conn);
+                dt = new DataTable();
+                dt.Load(cmd.ExecuteReader());
+                conn.Close();
+                Dgv_Sistem.DataSource = null;//reset
+                Dgv_Sistem.DataSource = dt;
+
+            }
+            catch (Exception ex)
+            {
+                conn.Close();
+                MessageBox.Show("ERROR : " + ex.Message);
+            }
+        }
+        private void Banka_Select()
+        {
+            try
+            {
+                conn.Open();
+                sql = @"SELECT * FROM banka_uyeleri ";
+
+                cmd = new NpgsqlCommand(sql, conn);
+                dt = new DataTable();
+                dt.Load(cmd.ExecuteReader());
+                conn.Close();
+                dgv_bankaUyeleri.DataSource = null;//reset
+                dgv_bankaUyeleri.DataSource = dt;
+
+            }
+            catch (Exception ex)
+            {
+                conn.Close();
+                MessageBox.Show("ERROR : " + ex.Message);
+            }
+        }
+
+        private void btn_Guncelle_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+                sql = @"SELECT * from update_banka_uyeleri(:_id,:_maas)";
+                cmd = new NpgsqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("_id", txtBox_ID.Text);
+                cmd.Parameters.AddWithValue("_maas", TxtBox_Maas.Text);
+                int result = (int)cmd.ExecuteScalar();
+                conn.Close();
+                if (result == 1)
+                {
+                    MessageBox.Show("Güncelleme başarılı.");
+                    Select_KrediTur();
+
+                }
+                else
+                {
+                    MessageBox.Show("Güncelleme başarısız.");
+                }
+            }
+            catch (Exception ex)
+            {
+                conn.Close();
+                MessageBox.Show("Güncelleme Başarısız. Error: " + ex.Message);
+            }
+        }
+        private void insert_sis()
+        {
+
         }
     }
 }
